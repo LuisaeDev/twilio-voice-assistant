8f58c3f8502a268921f133df6929c9ad6561cf55
Luis Aguilar <hi@luisae.dev>
2024-01-17 01:02:28 -0600

    feat: register services at laravel service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Register the Twilio service
+        $this->app->singleton(
+            \App\Contracts\TwilioClient::class,
+            \App\Services\TwilioClient::class
+        );
+
+        // Register the Voice-Assistant-Handlers service
+        $this->app->bind(
+            \App\Contracts\VoiceAssistantHandlers::class,
+            \App\Services\VoiceAssistantHandlers::class
+        );
+
+        // Register the Voice-Assistant-Responses service
+        $this->app->bind(
+            \App\Contracts\VoiceAssistantResponses::class,
+            \App\Services\VoiceAssistantResponses::class
+        );
+
+        // Register the Agents service
+        $this->app->bind(
+            \App\Contracts\AgentsManager::class,
+            \App\Services\AgentsManager::class
+        );
     }
 
     /**
