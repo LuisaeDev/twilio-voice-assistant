ce6e44f42fe9f39728512bf06bb47d7656fbd594
Luis Aguilar <hi@luisae.dev>
2024-01-17 01:04:54 -0600

    feat: update relations at user's model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,4 +43,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Relationship with user.
+     *
+     * @return HasOne
+     */
+    public function agent(): HasOne
+    {
+        return $this->hasOne(Agent::class, 'id', 'id');
+    }
 }
